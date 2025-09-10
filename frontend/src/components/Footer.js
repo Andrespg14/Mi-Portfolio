6342f716fd926d0210014830b0e882091bf59146
Andrespg14 <85160387+Andrespg14@users.noreply.github.com>
2025-09-10 15:22:16 +0200

    Cambios estéticos y footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
     return (
-        <footer style={{ textAlign: 'center', padding: '1rem'}}>
-            <p>&copy; {new Date().getFullYear()} Mi Portfolio. Todos los derechos reservados.</p>
+        <footer className="footer">
+            <a href="https://www.linkedin.com/in/andr%C3%A9s-p%C3%A9rez-garc%C3%ADa-261934180/" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+            </a>
+            {' | '}
+            <a href="https://github.com/Andrespg14" target="_blank" rel="noopener noreferrer">
+                GitHub
+            </a>
+            <p>© 2024 Andrés. Todos los derechos reservados.</p>
         </footer>
     );
 }
