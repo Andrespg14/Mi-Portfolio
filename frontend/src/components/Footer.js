7091390fe3bc45e95228ed8e7bc7caeb7b8ec8e9
Andrespg14 <85160387+Andrespg14@users.noreply.github.com>
2025-09-10 15:51:46 +0200

    Footer v1

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,20 +2,24 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
+
     return (
         <footer className="footer">
             <a href="https://www.linkedin.com/in/andr%C3%A9s-p%C3%A9rez-garc%C3%ADa-261934180/" target="_blank" rel="noopener noreferrer">
-                LinkedIn
+                <i className="fab fa-linkedin"></i> LinkedIn
             </a>
             {' | '}
             <a href="https://github.com/Andrespg14" target="_blank" rel="noopener noreferrer">
-                GitHub
+                <i className="fab fa-github"></i> GitHub
             </a>
-            <p>© 2024 Andrés. Todos los derechos reservados.</p>
+            {' | '}
+            <a href='mailto:anpergar14@gmail.com'>
+                <i className="fas fa-envelope"></i> anpergar14@gmail.com
+            </a>
+            <p>© 2025 Andrés Pérez García</p>
         </footer>
     );
 }
 
-// TODO: Add content for the Footer component
 
 export default Footer;
