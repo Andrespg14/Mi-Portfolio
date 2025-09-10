fe337edabd8699e32f2d19e02c13abb0eb46cbe4
Andrespg14 <85160387+Andrespg14@users.noreply.github.com>
2025-09-10 22:15:02 +0200

    feat (pagina proyectos): añadir cards de proyectos

--- frontend/src/pages/Proyectos.js
+++ frontend/src/pages/Proyectos.js
@@ -1,6 +1,28 @@
 import React from "react";
+import './Proyectos.css'
 
 export default function Proyectos() {
-    return <h1>Proyectos</h1>;
-    // Todo: Add content for the Proyectos page
+    return (
+    <div className="div-proyectos-padre">
+        <div className="container-proyectos">
+            <div className="card" style={{"width": "40rem"}}>
+            <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card" style={{"width": "40rem"}}>
+            <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    );
 }
