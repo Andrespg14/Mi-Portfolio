340b53d254234e52d45ff36fd2779ae84c107f9e
Andrespg14 <85160387+Andrespg14@users.noreply.github.com>
2025-09-22 18:24:21 +0200

    feat: Add hamburguer menu incon and auto close

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,11 @@ import './Header.css';
 
 function Header() {
   const location = useLocation(); // Hook to get the current location
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false); // Close the navbar when a link is clicked
+  }
 
   let pageName = '';
   switch (location.pathname) {
@@ -23,16 +28,16 @@ function Header() {
       pageName = '';
   } 
   return (
-    <Navbar expand="lg" sticky="top" className="header-navbar">
+    <Navbar expand="lg" sticky="top" className="header-navbar" expanded={expanded}>
         <Container fluid className='d-flex'> 
-          <Navbar.Toggle aria-controls='basic-navbar-nav' as="button">
-            <i className="fa-solid fa-house"></i>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' as="button" onClick={() => setExpanded(!expanded)}>
+            <i className="fa-solid fa-bars"></i>
           </Navbar.Toggle>
           <span className='current-page-mobile ms-2'>
             {pageName}
           </span>
             <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='mx-auto' activeKey={window.location.pathname}>
+                <Nav className='mx-auto' activeKey={window.location.pathname} onClick={handleNavClick}>
                   <Nav.Item>
                     <NavLink className="nav-link" to="">Inicio</NavLink>
                   </Nav.Item>
